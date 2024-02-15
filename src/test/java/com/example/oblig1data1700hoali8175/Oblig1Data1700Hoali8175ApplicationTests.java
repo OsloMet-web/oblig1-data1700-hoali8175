6a50e9075565871815c7ecc7f1bd7b9369f8df6b
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-02-15 17:53:10 +0100

    Oppretting av SpringBoot prosjekt og oppdatering av readme fil

---
+++ src/test/java/com/example/oblig1data1700hoali8175/Oblig1Data1700Hoali8175ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700hoali8175;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Data1700Hoali8175ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
