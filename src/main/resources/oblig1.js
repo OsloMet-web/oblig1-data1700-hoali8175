4384364836b2ba94f8c8d559fb79d8e6512e81ba
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-02-15 18:25:38 +0100

    la til js kode for feilmedlinger

--- src/main/resources/oblig1.js
+++ src/main/resources/oblig1.js
@@ -5,3 +5,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Array for å lagre alle kinobilletter
     const billetter = [];
+
+    // Lytter etter innlevering av skjemaet for kinobillettbestilling
+    billettForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // Forhindrer standard oppførsel ved innlevering av registrering
+
+        // Henter verdier fra input feltene
+        const filmNavn = document.getElementById('filmNavn').value.trim();
+        const antallBilletter = document.getElementById('antallBilletter').value.trim();
+        const kundeNavn = document.getElementById('kundeNavn').value.trim();
+        const kundeEtternavn = document.getElementById('kundeEtternavn').value.trim();
+        const kundeNummer = document.getElementById('kundeNummer').value.trim();
+        const kundeEmail = document.getElementById('kundeEmail').value.trim();
+
+        // Henter referanser til feilmeldings-elementene
+        const filmNavnError = document.getElementById('filmNavnError');
+        const antallBilletterError = document.getElementById('antallBilletterError');
+        const kundeNavnError = document.getElementById('kundeNavnError');
+        const kundeEtternavnError = document.getElementById('kundeEtternavnError');
+        const kundeNummerError = document.getElementById('kundeNummerError');
+        const kundeEmailError = document.getElementById('kundeEmailError');
