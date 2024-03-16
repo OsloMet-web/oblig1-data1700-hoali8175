cabe4d13d2422e438fe8878b3c4a0f9262f159a3
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-16 16:56:56 +0100

    .

---
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/BilettApplication2.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700hoali8175.oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilettApplication2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilettApplication2.class, args);
+    }
+
+}
