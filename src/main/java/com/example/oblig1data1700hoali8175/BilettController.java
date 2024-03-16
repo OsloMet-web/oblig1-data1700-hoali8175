139ad420def499acfd0bca28aaed74883b131e59
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-16 15:34:29 +0100

    .

--- src/main/java/com/example/oblig1data1700hoali8175/BilettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/BilettController.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oblig1Data1700Hoali8175Application {
+public class BilettController {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oblig1Data1700Hoali8175Application.class, args);
+        SpringApplication.run(BilettController.class, args);
     }
 
 }
