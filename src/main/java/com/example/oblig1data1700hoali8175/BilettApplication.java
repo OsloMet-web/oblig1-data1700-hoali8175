a2c57d7c4785bb13c0716eafaeff4ea343f7cbb7
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-16 15:36:57 +0100

    .

---
+++ src/main/java/com/example/oblig1data1700hoali8175/BilettApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700hoali8175;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilettApplication.class, args);
+    }
+
+}
--- src/main/java/com/example/oblig1data1700hoali8175/BilettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/BilettController.java
@@ -1,13 +1,27 @@
 package com.example.oblig1data1700hoali8175;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@SpringBootApplication
+import java.util.ArrayList;
+
+
+@RestController
+@RequestMapping("/api/tickets")
 public class BilettController {
+    private ArrayList<Bilett> biletter = new ArrayList<>();
 
-    public static void main(String[] args) {
-        SpringApplication.run(BilettController.class, args);
+    @PostMapping("/book")
+    public String bookTicket(@RequestBody Bilett bilett) {
+        biletter.add(bilett);
+        return ("Ticket booked successfully!");
     }
 
+    @GetMapping("/all")
+    public ArrayList<Bilett> getAllTickets() {
+        return biletter;
+    }
 }
