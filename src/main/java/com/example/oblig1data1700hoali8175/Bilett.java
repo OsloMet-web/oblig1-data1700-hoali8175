8e6dfb54810478272a683604fabbb590bcbd5cbf
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-16 15:37:44 +0100

    .

---
+++ src/main/java/com/example/oblig1data1700hoali8175/Bilett.java
@@ -0,0 +1,10 @@
+package com.example.oblig1data1700hoali8175;
+
+public class Bilett {
+    private String filmNavn;
+    private int antallBiletter;
+    private String kundeNavn;
+    private String kundeEtternavn;
+    private String kundeNummer;
+    private String kundeEmail;
+}
