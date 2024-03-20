0d6c2f8446fadf22e6fdd7d2d3371b0fe82d5c58
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-20 22:22:02 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/Billett.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/Billett.java
@@ -9,5 +9,56 @@ public class Billett {
     private String kundeNummer;
     private String kundeEmail;
 
-    // Konstruktør, getters og setters
+    // Konstruktør
+    public Billett() {}
+
+    // Getters og setters for alle feltene
+    public String getFilmNavn() {
+        return filmNavn;
+    }
+
+    public void setFilmNavn(String filmNavn) {
+        this.filmNavn = filmNavn;
+    }
+
+    public int getAntallBilletter() {
+        return antallBilletter;
+    }
+
+    public void setAntallBilletter(int antallBilletter) {
+        this.antallBilletter = antallBilletter;
+    }
+
+    public String getKundeNavn() {
+        return kundeNavn;
+    }
+
+    public void setKundeNavn(String kundeNavn) {
+        this.kundeNavn = kundeNavn;
+    }
+
+    public String getKundeEtternavn() {
+        return kundeEtternavn;
+    }
+
+    public void setKundeEtternavn(String kundeEtternavn) {
+        this.kundeEtternavn = kundeEtternavn;
+    }
+
+    public String getKundeNummer() {
+        return kundeNummer;
+    }
+
+    public void setKundeNummer(String kundeNummer) {
+        this.kundeNummer = kundeNummer;
+    }
+
+    public String getKundeEmail() {
+        return kundeEmail;
+    }
+
+    public void setKundeEmail(String kundeEmail) {
+        this.kundeEmail = kundeEmail;
+    }
 }
+
