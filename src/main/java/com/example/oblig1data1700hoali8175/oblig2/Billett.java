501dcd7201c223bedcf220cfd8e60746a99b308f
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-20 21:46:23 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/Billett.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/Billett.java
@@ -1,10 +1,64 @@
 package com.example.oblig1data1700hoali8175.oblig2;
 
 public class Billett {
+
     private String filmNavn;
-    private int antallBiletter;
+    private int antallBilletter;
     private String kundeNavn;
     private String kundeEtternavn;
     private String kundeNummer;
     private String kundeEmail;
+
+    // Standardkonstruktør
+    public Billett() {
+    }
+
+    // Gettere og settere
+    public String getFilmNavn() {
+        return filmNavn;
+    }
+
+    public void setFilmNavn(String filmNavn) {
+        this.filmNavn = filmNavn;
+    }
+
+    public int getAntallBilletter() {
+        return antallBilletter;
+    }
+
+    public void setAntallBilletter(int antallBilletter) {
+        this.antallBilletter = antallBilletter;
+    }
+
+    public String getKundeNavn() {
+        return kundeNavn;
+    }
+
+    public void setKundeNavn(String kundeNavn) {
+        this.kundeNavn = kundeNavn;
+    }
+
+    public String getKundeEtternavn() {
+        return kundeEtternavn;
+    }
+
+    public void setKundeEtternavn(String kundeEtternavn) {
+        this.kundeEtternavn = kundeEtternavn;
+    }
+
+    public String getKundeNummer() {
+        return kundeNummer;
+    }
+
+    public void setKundeNummer(String kundeNummer) {
+        this.kundeNummer = kundeNummer;
+    }
+
+    public String getKundeEmail() {
+        return kundeEmail;
+    }
+
+    public void setKundeEmail(String kundeEmail) {
+        this.kundeEmail = kundeEmail;
+    }
 }
--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
@@ -1,22 +1,27 @@
 package com.example.oblig1data1700hoali8175.oblig2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/billetter")
 public class BillettController {
 
-    private final ArrayList<Billett> billetter = new ArrayList<>();
+    @Autowired
+    private BillettService billettService;
 
     @PostMapping("/registrer")
-    public void registrerBillett(@RequestBody Billett billett) {
-        billetter.add(billett);
+    public ResponseEntity<?> registrerBillett(@RequestBody Billett billett) {
+        billettService.registrerBillett(billett);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/hentAlle")
-    public ArrayList<Billett> hentAlleBilletter() {
-        return billetter;
+    public ResponseEntity<List<Billett>> hentAlleBilletter() {
+        List<Billett> billetter = billettService.hentAlleBilletter();
+        return new ResponseEntity<>(billetter, HttpStatus.OK);
     }
 }
