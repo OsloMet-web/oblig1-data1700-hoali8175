9130f875f78b350ebb0d4f673a6c379faee0fdb9
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-20 22:41:11 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
@@ -1,30 +1,28 @@
 package com.example.oblig1data1700hoali8175.oblig2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/billetter")
+@RequestMapping("/api/billetter") // Endre URL-mapping til dette nivået
 public class BillettController {
 
-    private final List<Billett> billetter = new ArrayList<>();
+    @Autowired
+    private BillettService billettService;
 
-    @PostMapping("/registrer")
-    public ResponseEntity<String> registrerBillett(@RequestBody Billett billett) {
-        billetter.add(billett);
-        return new ResponseEntity<>("Billett registrert", HttpStatus.CREATED);
+    @PostMapping("/registrer") // Endre URL-mapping til dette nivået
+    public ResponseEntity<?> registrerBillett(@RequestBody Billett billett) {
+        billettService.registrerBillett(billett);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/hentAlle")
+    @GetMapping("/hentAlle") // Endre URL-mapping til dette nivået
     public ResponseEntity<List<Billett>> hentAlleBilletter() {
+        List<Billett> billetter = billettService.hentAlleBilletter();
         return new ResponseEntity<>(billetter, HttpStatus.OK);
     }
 }
-
-
-
-
