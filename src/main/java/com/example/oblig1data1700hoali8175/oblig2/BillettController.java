0bd1297c61fe76c3e17d54f812dbf67f597413d9
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-20 23:35:18 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
@@ -1,29 +1,31 @@
 package com.example.oblig1data1700hoali8175.oblig2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/billetter")
 public class BillettController {
 
-    private final List<Billett> billetter;
+    // Simulert lagring av billetter i minnet
+    private List<Billett> billetter = new ArrayList<>();
 
-    public BillettController(List<Billett> billetter) {
-        this.billetter = billetter;
-    }
-
+    // Metode for å registrere en billett
     @PostMapping("/registrer")
     public void registrerBillett(@RequestBody Billett billett) {
         billetter.add(billett);
     }
 
+    // Metode for å hente alle billetter
     @GetMapping("/hentAlle")
     public List<Billett> hentAlleBilletter() {
         return billetter;
     }
 
+    // Metode for å slette alle billetter
     @DeleteMapping("/slettAlle")
     public void slettAlleBilletter() {
         billetter.clear();
