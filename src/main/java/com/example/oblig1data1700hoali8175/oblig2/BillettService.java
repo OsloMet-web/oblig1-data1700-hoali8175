b4cfc96d6bf6d91fc2d061712bc44bec3012242a
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-20 22:04:43 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettService.java
+++
@@ -1,9 +0,0 @@
-package com.example.oblig1data1700hoali8175.oblig2;
-
-import java.util.List;
-
-public interface BillettService {
-    void registrerBillett(Billett billett);
-    List<Billett> hentAlleBilletter();
-}
-
