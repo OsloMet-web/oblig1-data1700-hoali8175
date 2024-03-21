327310bccfc1c91b6756a56d0b5de0336fa33edb
HokamAli <143800231+HokamAli@users.noreply.github.com>
2024-03-21 17:18:53 +0100

    style med bootstrap

--- src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
+++ src/main/java/com/example/oblig1data1700hoali8175/oblig2/BillettController.java
@@ -3,7 +3,7 @@ package com.example.oblig1data1700hoali8175.oblig2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import webprog4.Kunde2;
+
 
 import java.util.ArrayList;
 
